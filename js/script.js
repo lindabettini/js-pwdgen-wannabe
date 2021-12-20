2f6d26d6085b07efa5f24b6adac721160bdd12d8
lindabettini <linda.bettini@hotmail.it>
2021-12-20 14:44:54 +0100

    firts push

---
+++ js/script.js
@@ -0,0 +1,12 @@
+console.log('JS OK');
+
+/* 
+1. Chiedi all’utente il suo nome,
+2. Chiedi il suo cognome,
+3. Chiedi il suo colore preferito
+4. Infine stampa sulla pagina nomecognomecolorepreferito21
+*/
+
+// ç Chiediamo il nome 
+const name = prompt('Come ti chiami?');
+console.log(name);
