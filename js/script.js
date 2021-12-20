0b72a331cb5ab228d85c60d14134e241ddf3c063
lindabettini <linda.bettini@hotmail.it>
2021-12-20 15:23:22 +0100

    Better comment

--- js/script.js
+++ js/script.js
@@ -5,31 +5,33 @@ console.log('JS OK');
 2. Chiedi il suo cognome,
 3. Chiedi il suo colore preferito
 4. Aggiungo anno alla fine della password
-5. Infine stampa sulla pagina nomecognomecolorepreferito21
+5. Assemblo password
+6. Recupero elemento in cui stampare
+7. Infine stampa sulla pagina nomecognomecolorepreferito21
 */
 
-// Chiediamo il nome 
+// 1.Chiediamo il nome 
 const userName = prompt('Come ti chiami?');
 console.log(userName);
 
-// Chiediamo il cognome 
+// 2.Chiediamo il cognome 
 const userSurname = prompt('Quale è il tuo cognome?');
 console.log(userSurname);
 
-// Chiediamo il colore preferito
+// 3.Chiediamo il colore preferito
 const userFavColor = prompt('Quale è il tuo colore preferito?');
 console.log(userFavColor);
 
-// aggiungiamo anno alla fine della password
+// 4.aggiungiamo anno alla fine della password
 const year = '21';
 console.log(year);
 
-// Creiamo la password
+// 5.Creiamo la password
 const userPassword = userName + userSurname + userFavColor + year;
 console.log(userPassword);
 
-// Recupero elemento in cui stampare
+// 6.Recupero elemento in cui stampare
 const finalpasswordElement = document.getElementById('finalpassword');
 
-// Stampiamo la password
+// 7.Stampiamo la password
 finalpasswordElement.innerText = userPassword;
